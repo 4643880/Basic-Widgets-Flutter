97d591638f56ff597db28ab4a516d2105c4b33dd
Aizaz Haider <aizazisonline@gmail.com>
2021-10-06 20:51:29 +0500

    Media Query

--- lib/pages/practice.dart
+++ lib/pages/practice.dart
@@ -31,31 +31,10 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
-        alignment: Alignment.center,
-        clipBehavior: Clip.none,
-        children: [        
-          Container(
-            height: coverimg_height,
-            child: Image.network("https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80", fit: BoxFit.cover,),
-          ),
-          Positioned(            
-            top: coverimg_height - profileimg_height /2,            
-            child: Container(
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(100)
-              ),
-              
-              child: CircleAvatar(
-                radius: profileimg_height / 2,
-                backgroundColor: Colors.red,
-                backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2017/04/28/21/34/stylish-boy-2269500_1280.jpg"),
-              ),
-            ),
-          ),
-        ],
+      body:Container(
+        height: MediaQuery.of(context).size.height*.5,
+        width: MediaQuery.of(context).size.width*.5,
+        color: Colors.red,
       ),
     );
   }
