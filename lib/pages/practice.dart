15c09da5572039e45dde36d31542e85f4341809d
Aizaz Haider <aizazisonline@gmail.com>
2021-10-10 11:13:46 +0500

    GridView.Count

--- lib/pages/practice.dart
+++ lib/pages/practice.dart
@@ -24,40 +24,34 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> {  
+  var mylist = [1,2,3,4,5,6,7,8];
+  // var myimages = ["https://bit.ly/3ap5EX7", "as"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(children: [
-        abc(Colors.lightBlue, "Blue", "This is demo Description"),
-        abc(Colors.lightGreen, "Green", "This is demo Description"),
-        abc(Colors.grey, "Grey", "This is demo Description"),
-        abc(Colors.red, "Red", "This is demo Description"),
-        abc(Colors.orange, "Red", "This is demo Description")
-      ],)
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 18,
+          mainAxisSpacing: 18,
+          children: List.generate(mylist.length, (index) {
+            return Container(
+                height: 100,
+                width: 100,
+                color: Colors.lime,
+                child: Center(child: Text(mylist[index].toString(), style: TextStyle(fontSize: 30, color: Colors.white),)),
+                );            
+          })
+          ),
+      )
     );
   }
 
 }
 
-Widget abc(Color color, var name, var desc){
-  return Container(
-          color: color,
-          width: 100,
-           height: 150,
-           child: Row(
-             children: [
-               Padding(
-                 padding: const EdgeInsets.all(32.0),
-                 child: Column(
-                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                   children: [
-                   Text(name, style: TextStyle(fontSize: 30),),
-                   Text(desc, style: TextStyle(fontSize: 18),),
-                 ],),
-               )
-             ],             
-          ),
-        );
-}
+
+
+
